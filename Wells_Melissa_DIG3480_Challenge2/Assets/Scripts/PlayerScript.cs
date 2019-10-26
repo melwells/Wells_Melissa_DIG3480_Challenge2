2a551e2c57ed61d23568b2f69d13247f7c2d8195
melwells <55035851+melwells@users.noreply.github.com>
2019-10-26 19:07:56 -0400

    Update

--- Wells_Melissa_DIG3480_Challenge2/Assets/Scripts/PlayerScript.cs
+++ Wells_Melissa_DIG3480_Challenge2/Assets/Scripts/PlayerScript.cs
@@ -7,14 +7,20 @@ public class PlayerScript : MonoBehaviour
 {
     private Rigidbody2D rd2d;
     public float speed;
-    public Text score;
-    private int scoreValue = 0;
+    public Text score; //game score
+    public Text livesText; //lives text
+    public Text winText; //win text
+
+    private int scoreValue = 0; //score deafault
+    private int livesValue = 3; //lives default
 
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
-        score.text = scoreValue.ToString();
+        score.text = "Score: " + scoreValue.ToString();
+        winText.text = "";
+        livesText.text = "Lives: " + livesValue.ToString();
     }
 
     public void Update()
@@ -37,10 +43,30 @@ public class PlayerScript : MonoBehaviour
        if (collision.collider.tag == "Coin")
         {
             scoreValue += 1;
-            score.text = scoreValue.ToString();
+            score.text = "Score: " + scoreValue.ToString();
             Destroy(collision.collider.gameObject);
         }
 
+        if (scoreValue == 4)
+        transform.position = new Vector2(46.03f, -0.19f);
+
+        if (scoreValue >= 8)
+            //... then set the text property of our winText object to "You win!"
+            winText.text = "You win! Game created by Melissa Wells.";
+
+        else if (collision.gameObject.CompareTag("Enemy")) {
+            collision.gameObject.SetActive(false);
+            //count = count - 1; subtract one point from countText
+            livesValue = livesValue - 1;
+            SetLivesText();
+
+    }
+
+    void SetLivesText()
+    {
+        //Set the text property of our our countText object to "Count: " followed by the number stored in our count variable.
+        livesText.text = "Lives: " + livesValue.ToString ();
+        }
     }
 
     private void OnCollisionStay2D(Collision2D collision)
